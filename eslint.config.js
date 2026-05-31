8d029c9168e73ea68e911b50478e1551bb312ba3
sar <frederic.sar@etu.umontpellier.fr>
2026-05-31 19:36:11 +0200

    feat: API calculatrice avec tests Jest et CI GitHub Actions

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,30 @@
 const js = require("@eslint/js");
-const globals = require("globals"); // 1. On importe le package de globales d'ESLint
+
 module.exports = [
     js.configs.recommended,
     {
         languageOptions: {
             ecmaVersion: 2022,
             globals: {
+                // Environnement Node.js (Plus besoin de l'import 'globals')
                 require: "readonly",
                 module: "readonly",
                 exports: "readonly",
                 process: "readonly",
                 console: "readonly",
+                __dirname: "readonly",
+                __filename: "readonly",
+
+                // Environnement Jest (Comme ça, plus besoin des commentaires dans les tests)
+                describe: "readonly",
+                test: "readonly",
+                it: "readonly",
+                expect: "readonly",
+                beforeEach: "readonly",
+                beforeAll: "readonly",
+                afterEach: "readonly",
+                afterAll: "readonly",
+                jest: "readonly"
             },
         },
         rules: {
